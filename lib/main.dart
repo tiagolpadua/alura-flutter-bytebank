3b22fb5bf5d044746754ac24d42d3cea11941154
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-07-15 22:42:43 -0300

    a01a08

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,11 @@ void main() => runApp(Column(
           'Craft beautiful UIs',
           textDirection: TextDirection.ltr,
         ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children: <Widget>[
-            Text(
-              'Deliver features faster',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
+        Expanded(
+          child: FittedBox(
+            fit: BoxFit.contain, // otherwise the logo will be tiny
+            child: const FlutterLogo(),
+          ),
         ),
       ],
     ));
