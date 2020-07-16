de2c192bf7883c3efdcd9dd572c78a2eebeabd8d
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-07-15 23:11:20 -0300

    a02a06

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('200.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencias(),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
@@ -28,3 +11,40 @@ void main() => runApp(MaterialApp(
         ),
       ),
     ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(children: <Widget>[
+      ItemTransferencia(Transferencia(100.0, 1000)),
+      ItemTransferencia(Transferencia(200.0, 2000)),
+      ItemTransferencia(Transferencia(300.0, 3000)),
+    ]);
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
